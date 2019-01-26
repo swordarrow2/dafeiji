fbbe785c106477c2d2dea0092f1e80f340b27614
swordarrow2 <2856986197@qq.com>
2019-01-26 15:27:47 +0800

    整理代码

--- SSTTGG/app/src/main/java/com/meng/stg/helpers/mbgparser/Layer.java
+++ SSTTGG/app/src/main/java/com/meng/stg/helpers/mbgparser/Layer.java
@@ -1,107 +1,76 @@
 package com.meng.stg.helpers.mbgparser;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
-
 import com.meng.stg.helpers.mbgparser.item.*;
+import java.io.*;
+import java.util.*;
+
 import static com.meng.stg.helpers.mbgparser.MBGUtils.*;
 
-public class Layer implements Serializable
-{
-	private static final long serialVersionUID = 3131693177240044800L;
+public class Layer implements Serializable{
+
 	public String name;
 	public Life life = new Life();
-
 	public List<BulletEmitter> BulletEmitters = new ArrayList<>();
 	public List<ReflexBoard> ReflexBoards = new ArrayList<>();
 	public List<ForceField> ForceFields = new ArrayList<>();
 	public List<Mask> Masks = new ArrayList<>();
 	public List<LazerEmitter> LazerEmitters = new ArrayList<>();
 
-	private void LoadContent(
-		BufferedReader mbg,
-		int bulletEmitterCount,
-		int lazerEmitterCount,
-		int maskEmitterCount,
-		int reflexBoardCount,
-		int forceFieldCount) throws IOException
-	{
+	private void LoadContent(BufferedReader mbg,int bulletEmitterCount,int lazerEmitterCount,int maskEmitterCount,int reflexBoardCount,int forceFieldCount) throws IOException{
 		List<Action> linkers = new ArrayList<Action>();
-
-		BulletEmitters = new ArrayList<BulletEmitter>();
-		for (int i = 0; i < bulletEmitterCount; ++i)
-		{
-			Tuple<BulletEmitter, Action> result = BulletEmitter.parseFrom(mbg.readLine(), this);
+		BulletEmitters=new ArrayList<BulletEmitter>();
+		for(int i = 0; i<bulletEmitterCount; ++i){
+			Tuple<BulletEmitter, Action> result = BulletEmitter.parseFrom(mbg.readLine(),this);
 			linkers.add(result.Item2);
 			BulletEmitters.add(result.Item1);
-		}
-
-
-		LazerEmitters = new ArrayList<LazerEmitter>();
-		for (int i = 0; i < lazerEmitterCount; ++i)
-		{
+		  }
+		LazerEmitters=new ArrayList<LazerEmitter>();
+		for(int i = 0; i<lazerEmitterCount; ++i){
 			Tuple<LazerEmitter, Action> result = LazerEmitter.parseFrom(mbg.readLine(),this);
 			linkers.add(result.Item2);
 			LazerEmitters.add(result.Item1);
-		}
-
-		Masks = new ArrayList<Mask>();
-		for (int i = 0; i < maskEmitterCount; ++i)
-		{
+		  }
+		Masks=new ArrayList<Mask>();
+		for(int i = 0; i<maskEmitterCount; ++i){
 			Tuple<Mask, Action> result = Mask.parseFrom(mbg.readLine(),this);
 			linkers.add(result.Item2);
 			Masks.add(result.Item1);
-		}
-
-		ReflexBoards = new ArrayList<ReflexBoard>();
-		for (int i = 0; i < reflexBoardCount; ++i)
+		  }
+		ReflexBoards=new ArrayList<ReflexBoard>();
+		for(int i = 0; i<reflexBoardCount; ++i){
 			ReflexBoards.add(ReflexBoard.parseFrom(mbg.readLine()));
-
-		ForceFields = new ArrayList<ForceField>();
-		for (int i = 0; i < forceFieldCount; ++i)
+		  }
+		ForceFields=new ArrayList<ForceField>();
+		for(int i = 0; i<forceFieldCount; ++i){
 			ForceFields.add(ForceField.parseFrom(mbg.readLine()));
-
-		for (Action l : linkers)
+		  }
+		for(Action l : linkers){
 			l.invoke();
-	}
+		  }
+	  }
+	public BulletEmitter findBulletEmitterByID(int id){
+		for(BulletEmitter i : BulletEmitters)
+		  if(i.ID==id)
+			return i;
+		throw new IllegalArgumentException("找不到子弹发射器"+id);
+	  }
 
-	public BulletEmitter findBulletEmitterByID(int id)
-	{
-		for (BulletEmitter i : BulletEmitters)
-			if (i.ID == id)
-				return i;
-		throw new IllegalArgumentException("找不到子弹发射器" + id);
-	}
-
-	public static Layer parseFrom(String contentRaw, BufferedReader mbg) throws IOException
-	{
-		if (contentRaw.equals("empty"))
+	public static Layer parseFrom(String contentRaw,BufferedReader mbg) throws IOException{
+		if(contentRaw.equals("empty")){
 			return null;
-		else
-		{
+		  }else{
 			MRef<String> content = new MRef<String>(contentRaw);
 			Layer layer = new Layer();
-			layer.name = readString(content);
-			layer.life.begin = Integer.parseInt(readString(content));
-			layer.life.lifeTime = Integer.parseInt(readString(content));
+			layer.name=readString(content);
+			layer.life.begin=Integer.parseInt(readString(content));
+			layer.life.lifeTime=Integer.parseInt(readString(content));
 			int bulletEmitterCount = Integer.parseInt(readString(content));
 			int lazerEmitterCount = Integer.parseInt(readString(content));
 			int maskEmitterCount = Integer.parseInt(readString(content));
 			int reflexBoardCount = Integer.parseInt(readString(content));
 			int forceFieldCount = Integer.parseInt(readString(content));
-
-			layer.LoadContent(
-				mbg,
-				bulletEmitterCount,
-				lazerEmitterCount,
-				maskEmitterCount,
-				reflexBoardCount,
-				forceFieldCount);
-
+			layer.LoadContent(mbg,bulletEmitterCount,lazerEmitterCount,maskEmitterCount,reflexBoardCount,forceFieldCount);
 			return layer;
-		}
-	}
-}
+		  }
+	  }
+  }
