7fd5fd5d9847e1881af094fc25d1ea5846de598a
swordarrow2 <2856986197@qq.com>
2019-01-31 01:24:03 +0800

    多层弹幕实现

--- SSTTGG/app/src/main/java/com/meng/stg/bullets/BulletShooter.java
+++ SSTTGG/app/src/main/java/com/meng/stg/bullets/BulletShooter.java
@@ -22,6 +22,7 @@ public class BulletShooter{
     public static HashSet<BulletShooter> instances=new HashSet<BulletShooter>();
     public static LinkedBlockingQueue<BulletShooter> toDelete=new LinkedBlockingQueue<BulletShooter>();
     public static LinkedBlockingQueue<BulletShooter> toAdd=new LinkedBlockingQueue<BulletShooter>();
+	private int time=0;
     private Vector2 bulletCenter=new Vector2(200,200);
     private Vector2 bulletVelocity=new Vector2(0,-1);
     private BulletForm bf=BulletForm.lindan;
@@ -36,96 +37,95 @@ public class BulletShooter{
     public BulletShooter(BaseEnemyPlane baseEnemyPlane){
         this.baseEnemyPlane=baseEnemyPlane;
         toAdd.add(this);
-    }
+	  }
 
     public BulletShooter setStraightMove(boolean straightMove){
         this.straightMove=straightMove;
         return this;
-    }
+	  }
 
     public BulletShooter setMoveMethods(BaseMoveMethod... moveMethods){
         this.moveMethods=moveMethods;
         return this;
-    }
+	  }
 
     public BulletShooter setWays(int ways){
         this.ways=ways;
         return this;
-    }
+	  }
 
     public BulletShooter setCengShu(int cengShu){
         this.cengShu=cengShu;
         return this;
-    }
+	  }
 
     public BulletShooter setWaysDegree(int waysDegree){
         this.waysDegree=waysDegree;
         return this;
-    }
+	  }
 
     public BulletShooter setBulletCenter(Vector2 bulletCenter){
         this.bulletCenter=bulletCenter;
         return this;
-    }
+	  }
 
     public BulletShooter setBulletColor(BulletColor bc){
         this.bc=bc;
         return this;
-    }
+	  }
 
     public BulletShooter setBulletForm(BulletForm bf){
         this.bf=bf;
         return this;
-    }
+	  }
 
     public BulletShooter setBulletVelocity(Vector2 bulletVelocity){
         this.bulletVelocity=bulletVelocity;
         return this;
-    }
+	  }
 
     public BulletShooter setInFrame(int inFrame){
         this.inFrame=inFrame;
         return this;
-    }
+	  }
 
     public void update(){
+		++time;
         if(baseEnemyPlane.judgeCircle==null){
             kill();
-        }
-    }
+		  }
+	  }
 
     public void kill(){
         toDelete.add(this);
-    }
+	  }
 
     public static void updateAll(){
         while(!toDelete.isEmpty()){
             instances.remove(toDelete.poll());
-        }
+		  }
         while(!toAdd.isEmpty()){
             instances.add(toAdd.poll());
-        }
+		  }
         for(BulletShooter shooter : instances){
             shooter.update();
-        }
-    }
+		  }
+	  }
 
     public void shoot(){
         moveMethods=straightMove?new BaseMoveMethod[]{new MoveMethodStraight(inFrame,bulletVelocity)}:moveMethods;
         //    SimpleRedBullet.create(bulletCenter,bf,bc,moveMethods);
         //    SimpleRedBullet.create(bulletCenter,bf,bc,new MoveMethodStraight(inFrame,bulletVelocity));
-        for(int ceng=0;ceng<cengShu;++ceng){
+		float beilv=1;
+		for(int ceng=0;ceng<cengShu;++ceng){
 			float allAngle=(ways-1)*waysDegree;
-			Vector2 tmpv=bulletVelocity.cpy();
+			Vector2 tmpv=bulletVelocity.cpy().scl(beilv);
 			tmpv.rotate(-allAngle/2);
 			for(int i=0;i<ways;i++){
 				SimpleRedBullet.create(bulletCenter.cpy(),bf,bc,new MoveMethodStraight(inFrame,tmpv.cpy()));
 				tmpv.rotate(waysDegree);
 			  }
-		}
-     
-
-    }
-
-
-}
+			beilv+=0.1f;
+		  }
+	  }
+  }
