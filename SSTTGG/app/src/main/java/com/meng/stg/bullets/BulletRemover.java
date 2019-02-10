649822cb5e4c787262ba941d687a8b6d28f0b73c
swordarrow2 <2856986197@qq.com>
2019-02-11 00:05:05 +0800

    消弹全部由bulletRemover执行

--- SSTTGG/app/src/main/java/com/meng/stg/bullets/BulletRemover.java
+++ SSTTGG/app/src/main/java/com/meng/stg/bullets/BulletRemover.java
@@ -1,9 +1,8 @@
 package com.meng.stg.bullets;
 
-
 import com.badlogic.gdx.math.*;
 import com.meng.stg.*;
-import com.meng.stg.bullets.enemy.BulletKillMode;
+import com.meng.stg.bullets.enemy.*;
 import com.meng.stg.effects.enemy.*;
 import java.util.*;
 import java.util.concurrent.*;
@@ -13,25 +12,44 @@ public  class BulletRemover extends BaseGameObject{
 	public static HashSet<BulletRemover> instances=new HashSet<BulletRemover>();
     public static LinkedBlockingQueue<BulletRemover> toDelete=new LinkedBlockingQueue<BulletRemover>();
     public static LinkedBlockingQueue<BulletRemover> toAdd=new LinkedBlockingQueue<BulletRemover>();
+	private BulletKillMode bkm;
 
+	private int flag=0;
 	public void init(Vector2 center){
         existTime=0;
+		flag=0;
         toAdd.add(this);
         objectCenter.set(center);
         velocity.set(0,0);
-        judgeCircle=new Circle(objectCenter,1); //中心、半径
+        judgeCircle=new Circle(objectCenter,1);
+		bkm=BulletKillMode.KillOnBossLastDeath;
 	  }
 
     public void kill(){
 		toDelete.add(this);
 	  }
 
+	public void init(){
+		flag=1;
+		toAdd.add(this);
+        objectCenter.set(193,225);
+        velocity.set(0,0);
+        judgeCircle=new Circle(objectCenter,500);
+		bkm=BulletKillMode.killWithNothing;
+	  }
+
     public void update(){
         super.update();
-		judgeCircle.setRadius(existTime*7);
-		if(existTime>90){
-		  kill();
-		}
+		if(flag==0){
+			judgeCircle.setRadius(existTime*7);
+			if(existTime>90){
+				kill();
+			  }
+		  }else{
+			if(existTime>3){
+				kill();
+			  }
+		  }
 		judge();
 	  }
 
@@ -51,7 +69,7 @@ public  class BulletRemover extends BaseGameObject{
         for(BaseEnemyBullet baseBullet : BaseEnemyBullet.instances){
             if(judgeCircle.contains(baseBullet.objectCenter)){
 				Effect.create(baseBullet.objectCenter.cpy(),EffectType.explore);	
-                baseBullet.kill(BulletKillMode.KillOnBossLastDeath);
+                baseBullet.kill(bkm);
 			  }
 		  }
 	  }
