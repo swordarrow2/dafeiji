897f3e7b9f7c4c20f79c55481817d31582a058b4
swordarrow2 <2856986197@qq.com>
2019-04-08 12:21:44 +0800

    配置编辑器更新

--- guirenzhengxie/app/src/main/java/com/meng/guirenzhengxie/ConfigJavaBean.java
+++ guirenzhengxie/app/src/main/java/com/meng/guirenzhengxie/ConfigJavaBean.java
@@ -3,18 +3,11 @@ package com.meng.guirenzhengxie;
 import java.util.ArrayList;
 
 public class ConfigJavaBean {
-    public ArrayList<Long> mapGroupReply = new ArrayList<>();
+    public ArrayList<GroupReply> mapGroupReply = new ArrayList<>();
     public ArrayList<Long> mapQQNotReply = new ArrayList<>();
     public ArrayList<String> mapWordNotReply = new ArrayList<>();
     public ArrayList<String> mapGroupRepeater = new ArrayList<>();
     public ArrayList<Long> mapGroupDicReply = new ArrayList<>();
     public ArrayList<BilibiliUser> mapBiliUser = new ArrayList<>();
 
-    public class BilibiliUser {
-        public String name = "";
-        public long qq = 0;
-        public int bid = 0;
-        public int bliveRoom = 0;
-        public boolean autoTip = false;
-    }
 }
---
+++ guirenzhengxie/app/src/main/java/com/meng/guirenzhengxie/GroupReply.java
@@ -0,0 +1,7 @@
+package com.meng.guirenzhengxie;
+
+public class GroupReply {
+    public long groupNum = 0;
+    public boolean reply = true;
+}
+
