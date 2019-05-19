b265360115bb08bcacfcda7afe1c8264d5bf7d0e
swordarrow2 <2856986197@qq.com>
2019-05-19 22:38:27 +0800

    优化（？）

--- grzxmd/app/src/main/java/com/meng/grzxConfig/MaterialDesign/adapters/GroupConfigAdapter.java
+++ grzxmd/app/src/main/java/com/meng/grzxConfig/MaterialDesign/adapters/GroupConfigAdapter.java
@@ -10,83 +10,89 @@ import com.meng.grzxConfig.MaterialDesign.helpers.DownloadImageThread;
 import com.meng.grzxConfig.MaterialDesign.helpers.HeadType;
 import com.meng.grzxConfig.MaterialDesign.helpers.NetworkType;
 import com.meng.grzxConfig.MaterialDesign.javaBean.*;
+
 import java.io.*;
 import java.util.*;
 
 import com.meng.grzxConfig.MaterialDesign.activity.MainActivity;
+
 import android.app.*;
 
-public class GroupConfigAdapter extends BaseAdapter{
+public class GroupConfigAdapter extends BaseAdapter {
     private Activity context;
     private ArrayList<GroupConfig> groupReplies;
+    private HashMap<Long, Bitmap> hashMap = new HashMap<>();
 
-    public GroupConfigAdapter(Activity context, ArrayList<GroupConfig> groupReplies){
-        this.context=context;
-        this.groupReplies=groupReplies;
-	  }
+    public GroupConfigAdapter(Activity context, ArrayList<GroupConfig> groupReplies) {
+        this.context = context;
+        this.groupReplies = groupReplies;
+    }
 
-    public int getCount(){
+    public int getCount() {
         return groupReplies.size();
-	  }
+    }
 
-    public Object getItem(int position){
+    public Object getItem(int position) {
         return groupReplies.get(position);
-	  }
+    }
 
-    public long getItemId(int position){
+    public long getItemId(int position) {
         return groupReplies.get(position).hashCode();
-	  }
+    }
 
-    public View getView(final int position,View convertView,ViewGroup parent){
+    public View getView(final int position, View convertView, ViewGroup parent) {
 
         final ViewHolder holder;
-        if(convertView==null){
-            convertView=context.getLayoutInflater().inflate(R.layout.list_item_image_text_switch,null);
-            holder=new ViewHolder();
-            holder.groupNumber=(TextView) convertView.findViewById(R.id.group_reply_list_itemTextView);
-            holder.replySwitch=(Switch) convertView.findViewById(R.id.group_reply_list_itemSwitch);
-            holder.imageView=(ImageView) convertView.findViewById(R.id.group_reply_list_itemImageView);
+        if (convertView == null) {
+            convertView = context.getLayoutInflater().inflate(R.layout.list_item_image_text_switch, null);
+            holder = new ViewHolder();
+            holder.groupNumber = (TextView) convertView.findViewById(R.id.group_reply_list_itemTextView);
+            holder.replySwitch = (Switch) convertView.findViewById(R.id.group_reply_list_itemSwitch);
+            holder.imageView = (ImageView) convertView.findViewById(R.id.group_reply_list_itemImageView);
             convertView.setTag(holder);
-		  }else{
-            holder=(ViewHolder) convertView.getTag();
-		  }
+        } else {
+            holder = (ViewHolder) convertView.getTag();
+        }
         final GroupConfig groupReply = groupReplies.get(position);
         holder.groupNumber.setText(String.valueOf(groupReply.groupNumber));
         holder.replySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 
-			  @Override
-			  public void onCheckedChanged(CompoundButton p1,boolean p2){
-				  if(groupReply.reply!=p2){
-					  groupReply.reply=p2;
-					  ((MainActivity)context).networkManager.send(NetworkType.setGroup,position+" "+MainActivity.instence.gson.toJson(groupReply),GroupConfigAdapter.this);
-					}
-				}
-			});
+            @Override
+            public void onCheckedChanged(CompoundButton p1, boolean p2) {
+                if (groupReply.reply != p2) {
+                    groupReply.reply = p2;
+                    ((MainActivity) context).networkManager.send(NetworkType.setGroup, position + " " + MainActivity.instence.gson.toJson(groupReply), GroupConfigAdapter.this);
+                }
+            }
+        });
 
         holder.replySwitch.setChecked(groupReply.reply);
 
-        File imageFile = new File(MainActivity.instence.mainDic+"group/"+groupReply.groupNumber+".jpg");
-        if(imageFile.exists()){
-            holder.imageView.setImageBitmap(BitmapFactory.decodeFile(imageFile.getAbsolutePath()));
-		  }else{
-            if(MainActivity.instence.onWifi){
-                new DownloadImageThread(context,holder.imageView,groupReply.groupNumber, HeadType.QQGroup).start();
-			  }else{
-                holder.imageView.setImageResource(R.drawable.stat_sys_download_anim0);
-                holder.imageView.setOnClickListener(new View.OnClickListener() {
-					  @Override
-					  public void onClick(View v){
-						  new DownloadImageThread(context,holder.imageView,groupReply.groupNumber,HeadType.QQGroup).start();
-						}
-					});
-			  }
-		  }
+        if (hashMap.get(groupReply.groupNumber) == null) {
+            File imageFile = new File(MainActivity.instence.mainDic + "group/" + groupReply.groupNumber + ".jpg");
+            if (imageFile.exists()) {
+                hashMap.put(groupReply.groupNumber, BitmapFactory.decodeFile(imageFile.getAbsolutePath()));
+            } else {
+                if (MainActivity.instence.onWifi) {
+                    new DownloadImageThread(context, holder.imageView, groupReply.groupNumber, HeadType.QQGroup).start();
+                } else {
+                    holder.imageView.setImageResource(R.drawable.stat_sys_download_anim0);
+                    holder.imageView.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            new DownloadImageThread(context, holder.imageView, groupReply.groupNumber, HeadType.QQGroup).start();
+                        }
+                    });
+                }
+            }
+        }
+        holder.imageView.setImageBitmap(hashMap.get(groupReply.groupNumber));
         return convertView;
-	  }
+    }
 
-    private class ViewHolder{
+    private class ViewHolder {
         private ImageView imageView;
         private TextView groupNumber;
         private Switch replySwitch;
-	  }
-  }
+    }
+}
